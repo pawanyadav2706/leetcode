1b65e016969ea4577553826d7e1a8d30ec3d0385
pawanyadav2706 <pawanyadav12582@gmail.com>
2025-09-02 21:49:23 +0530

    Time: 30 ms (49.75%), Space: 44.8 MB (90.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            // formula
+            int complement = target - nums[i];
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == complement) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
