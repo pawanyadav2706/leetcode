466e2896879c8f80fd642dab55f2e871e4c5f89b
pawanyadav2706 <pawanyadav12582@gmail.com>
2026-06-13 11:08:23 +0530

    Time: 30 ms (41.3%), Space: 47.2 MB (23.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -12,12 +12,6 @@ class Solution {
         }
         return new int[]{}; 
     }
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-
-        int[] result = twoSum(nums, target);
-        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
-    }
 }
+
+   
